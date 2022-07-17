6ca391164c25a7829151082675cbf5170c9fe51a
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-07-17 02:04:59 +0000

    feat: add route to update order status

--- src/routes/orders.routes.ts
+++ src/routes/orders.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import { CreateOrderController } from '../modules/orders/useCases/createOrder/controller';
+import { UpdateOrderStatusController } from '../modules/orders/useCases/updateOrderStatus/controller';
 
 const ordersRouter = Router();
 
 const createOrderController = new CreateOrderController();
+const updateOrderStatusController = new UpdateOrderStatusController();
 
 ordersRouter.post('/', createOrderController.handle);
+ordersRouter.patch('/:id/status', updateOrderStatusController.handle);
 
 export { ordersRouter };
