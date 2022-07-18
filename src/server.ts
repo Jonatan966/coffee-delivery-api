f1e3bfe97d9ec51a89bf65c86f99ca7bc46c83ff
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-07-18 23:49:53 +0000

    feat: add swagger docs endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import 'dotenv/config';
 
+import swaggerUI from 'swagger-ui-express';
 import express from 'express';
 import 'express-async-errors';
 
@@ -9,12 +10,14 @@ import './shared/container';
 import { createConnection } from './shared/database';
 import { router } from './routes';
 import { errorHandler } from './middlewares/error-handler';
+import swaggerFile from './swagger.json';
 
 createConnection().then(() => console.log('[database] connected successful'));
 
 const app = express();
 
 app.use(express.json());
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 app.use(router);
 app.use(errorHandler);
 
