8065c2761c8245c1e31bb962173dcbc2d41bbc1a
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-07-20 00:13:48 +0000

    feat: use cors

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 
 import swaggerUI from 'swagger-ui-express';
 import express from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import './shared/container';
@@ -17,6 +18,7 @@ createConnection().then(() => console.log('[database] connected successful'));
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 app.use(router);
 app.use(errorHandler);
